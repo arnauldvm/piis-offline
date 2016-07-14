8fd408ed38b4573902d7d7c514c3cae5cf44f088
Arnauld Van Muysewinkel <Arnauld.VanMuysewinkel@smals.be>
2016-07-15 01:18:21 +0200

    Generate XML

--- src/app/js/piis.js
+++ src/app/js/piis.js
@@ -64,3 +64,75 @@ function addDynamicField(modelFieldId, inputFieldsIds, compute) {
   });
 }
 
+function initXmlDoc(rootElement) {
+  var xmlString = `<${rootElement}></${rootElement}>`;
+  var parser = new DOMParser();
+  var xmlDoc = parser.parseFromString(xmlString, "text/xml"); //important to use "text/xml"
+  return xmlDoc;
+}
+
+function nodeToString(xmlNode) {
+   return (new XMLSerializer()).serializeToString(xmlNode);
+}
+
+function toXml() {
+  var xmlDoc = initXmlDoc("piis");
+
+  function x() { // after http://stackoverflow.com/a/3191559
+    var node = xmlDoc.createElement(arguments[0]);
+
+    for(var i = 1; i < arguments.length; i++) {
+        child = arguments[i];
+        if (child === undefined || child === null) continue;
+        if(typeof child == 'string') {
+            child = xmlDoc.createTextNode(child);
+        }
+        node.appendChild(child);
+    }
+
+    return node;
+  }
+
+  function f(modelFieldId) {
+    return getModelField(modelFieldId).value;
+  }
+
+  xmlDoc.documentElement.appendChild(
+      x('beneficiary',
+        x('lastname', f('-beneficiary+lastname')),
+        x('firstname', f('-beneficiary+firstname')),
+        x('genre', f('-beneficiary+genre')),
+        x('nrn', f('-beneficiary+nrn')),
+        x('birthdate', f('-beneficiary+birthdate')),
+        x('address',
+          x('street', f('-beneficiary+address+street')),
+          x('streetnumber', f('-beneficiary+address+streetnumber')),
+          x('zip', f('-beneficiary+address+zip')),
+          x('locality', f('-beneficiary+address+locality'))
+         )
+       )
+  );
+  xmlDoc.documentElement.appendChild(
+      x('center',
+        x('municipality', f('-center+municipality')),
+        x('inscode', f('-center.inscode')),
+        x('president',
+          x('name', f('-center+president+name')),
+          x('genre', f('-center+president+genre'))
+         ),
+        x('director',
+          x('name', f('-center+director+name')),
+          x('genre', f('-center+director+genre'))
+         ),
+        x('address',
+          x('street', f('-center+address+street')),
+          x('streetnumber', f('-center+address+streetnumber')),
+          x('zip', f('-center+address+zip')),
+          x('locality', f('-center+address+locality'))
+         )
+       )
+  );
+
+  var xmlString = "<?xml version='1.0' ?>\n" + nodeToString(xmlDoc)
+  alert(xmlString);
+}
