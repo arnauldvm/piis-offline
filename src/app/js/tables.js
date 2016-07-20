f6a8bc3d53ba9fa7d97bf0fc9443482b6d7401cc
Arnauld Van Muysewinkel <Arnauld.VanMuysewinkel@smals.be>
2016-07-21 01:15:50 +0200

    feat(tables): add rows number

--- src/app/js/tables.js
+++ src/app/js/tables.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function updateRowIdx(tbody) {
+  for (let i = 0; i<tbody.rows.length; i++) {
+    tbody.rows[i].firstElementChild.textContent = i+1;
+  }
+}
+
 function addrow(tableId) {
   const table = document.getElementById(tableId);
   const numCols = table.tHead.rows[0].childElementCount;
@@ -11,9 +17,11 @@ function addrow(tableId) {
   tr.lastElementChild.textContent = "-";
   tr.lastElementChild.onclick = evt => removerow(tr);
   tbody.appendChild(tr);
+  updateRowIdx(tbody);
 }
 
 function removerow(tr) {
   const table = tr.parentNode.parentNode;
   table.deleteRow(tr.rowIndex);
+  updateRowIdx(table.tBodies[0]);
 }
